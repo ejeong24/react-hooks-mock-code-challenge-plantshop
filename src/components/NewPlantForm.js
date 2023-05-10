64355288041a180ebeb44f6d4d799665db1d9034
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-05-09 23:41:20 -0400

    swag

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ plants, setPlants}) {
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState(0)
+  const [image, setImage] = useState("")
+
+  function handleSubmit({plants}){ 
+   const updatedPlants = ([...plants, {
+      name: name,
+      price: price,
+      image: image
+    }])
+
+    setPlants(updatedPlants)
+    console.log(plants)
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input 
+        onChange={(e) => setName(e.target.value)}
+        type="text" 
+        name="name" 
+        placeholder="Plant name" 
+        value={name}
+        />
+        <input 
+        onChange={(e) => setImage(e.target.value)}
+        type="text" 
+        name="image" 
+        placeholder="Image URL" 
+        value={image}
+        />
+        <input 
+        onChange={(e) => setPrice(parseFloat(e.target.value))}
+        type="number" 
+        name="price" 
+        step="0.01" 
+        placeholder="Price" 
+        value={price}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((res) => res.json())
+    .then((plantsArray) => plantsArray.map((plant) => {
+      setPlants(plantsArray)
+    }))
+  },plants
+  )
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm/>
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
